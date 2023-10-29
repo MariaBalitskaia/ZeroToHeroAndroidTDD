9b1053b90de88ce6797ba2612d81aa904a14eb19
MariaBalitskaia <maria.m3@inbox.ru>
2023-10-30 00:06:12 +0300

    Task 10/Task010Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,43 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val ll: LinearLayout by lazy { findViewById(R.id.rootLayout) }
+    val tv: TextView by lazy { findViewById(R.id.titleTextView) }
+    val btn: Button by lazy { findViewById(R.id.removeButton) }
+
+    private var isButtonClicked: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            onButtonClicked()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("1", isButtonClicked)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val isButtonClicked = savedInstanceState.getBoolean("1")
+        if (isButtonClicked) {
+            onButtonClicked()
+        }
+    }
+
+    private fun onButtonClicked() {
+        isButtonClicked = true
+        ll.removeView(tv)
+        btn.isEnabled = false
     }
 }
